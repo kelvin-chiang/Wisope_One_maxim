538a4db2d294431609f2268464b3a2d057430676
kelvin-chiang <752025767@qq.com>
2020-01-27 21:28:29 +0800

    fourth commit

---
+++ maxim.py
@@ -0,0 +1,5 @@
+maxim_name = "kelvin"
+maxim_language = "python"
+maxim_sentence = "would you like to learn "
+maxim = "Hello " + maxim_name.title() +"," +maxim_sentence.title() + maxim_language.title() + " today!"
+print(maxim)
